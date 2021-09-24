c53b50cc030b6bb31e5c1c36efffb7a87ea09d36
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-09-24 16:00:45 +0700

    mainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,11 +5,89 @@
         style="max-width:1600px; margin:auto;"
         class=" bgmainpage shadow-10 "
       ></div>
-      <div class="leftdrawer shadow-3 ">
+      <div class="leftdrawer shadow-4 ">
         <!-- //****Menu */ -->
         <div>
-          <div>123</div>
-          <div>456</div>
+          <div class="row fontEdit items-center">
+            <img
+              src="../../public/images/logo.png"
+              style="width:88px;"
+              alt=""
+            />
+            <div class="q-pl-sm">
+              <div>{{ userName }}</div>
+              <div>{{ userType }}</div>
+            </div>
+          </div>
+          <div class="setMenu" v-show="menu == 1">
+            <img src="../../public/images/menu1red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 1" @click="menu = 1">
+            <img
+              src="../../public/images/menu1blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+
+          <div class="setMenu" v-show="menu == 2">
+            <img src="../../public/images/menu2red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 2" @click="menu = 2">
+            <img
+              src="../../public/images/menu2blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+          <div class="setMenu" v-show="menu == 3">
+            <img src="../../public/images/menu3red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 3" @click="menu = 3">
+            <img
+              src="../../public/images/menu3blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+          <div class="setMenu" v-show="menu == 4">
+            <img src="../../public/images/menu4red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 4" @click="menu = 4">
+            <img
+              src="../../public/images/menu4blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+          <div class="setMenu" v-show="menu == 5">
+            <img src="../../public/images/menu5red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 5" @click="menu = 5">
+            <img
+              src="../../public/images/menu5blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+          <div class="setMenu" v-show="menu == 6">
+            <img src="../../public/images/menu6red.png" alt="" />
+          </div>
+          <div class="setMenu" v-show="menu != 6" @click="menu = 6">
+            <img
+              src="../../public/images/menu6blue.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
+          <!-- exit btn  -->
+          <div class="setMenu exitBtn" @click="exitBtn">
+            <img
+              src="../../public/images/exit.png"
+              alt=""
+              class="cursor-pointer"
+            />
+          </div>
         </div>
         <!-- End of menu -->
       </div>
@@ -23,17 +101,28 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      menu: 1,
+      userName: "once",
+      userType: "ผู้ดูแลระบบ"
+    };
+  },
+  methods: {
+    exitBtn() {
+      this.$router.push("/");
+    }
   }
 };
 </script>
 <style lang="scss" scoped>
 .leftdrawer {
   margin: 10px;
+  margin-left: 30px;
   width: 300px;
   height: calc(100vh - 20px);
   background-color: rgba($color: #ffffff, $alpha: 0.5);
   border-radius: 30px;
+  border: 2px solid white;
 }
 .bgmain {
   background-image: url("../../public/images/bg.jpg");
@@ -42,5 +131,17 @@ export default {
 }
 .bgmainpage {
   background-color: rgba(12, 6, 44, 0.85);
+}
+.fontEdit {
+  padding-bottom: 20px;
+  font-size: 22px;
+  padding-top: 20px;
+}
+.setMenu {
+  position: relative;
+  left: -21px;
+}
+.exitBtn {
+  padding-top: 150px;
 }
 </style>
--- src/pages/loading.vue
+++ src/pages/loading.vue
@@ -1,11 +1,82 @@
 <template>
-  <div>
-    loading
+  <div class="bgmain fullscreen">
+    <div class="bigBox shadow-4 fixed-center" align="center">
+      <div class="q-pt-md">
+        <img src="../../public/images/logo.png" alt="" />
+      </div>
+      <div class="inputText q-mt-md">
+        <q-input
+          filled
+          v-model="userName"
+          label="ชื่อผู้ใช้งาน"
+          ref="usernameBlock"
+        />
+      </div>
+      <div class="inputText q-ma-md">
+        <q-input
+          v-model="password"
+          filled
+          label="รหัสผ่าน"
+          :type="isPwd ? 'password' : 'text'"
+          @keyup.enter="loginBtn"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+      </div>
+      <div class="q-pa-md">
+        <q-btn
+          style="background: #FFc24c;width:120px;height45px;"
+          label="เข้าสู่ระบบ"
+          @click="loginBtn"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      password: "",
+      isPwd: true,
+      userName: ""
+    };
+  },
+  methods: {
+    loginBtn() {
+      this.$router.push("category");
+    }
+  },
+  mounted() {
+    this.$refs.usernameBlock.$el.focus();
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bgmain {
+  background-image: url("../../public/images/bg.jpg");
+  background-size: 100%;
+  background-attachment: fixed;
+}
+.bigBox {
+  background-color: white;
+  border-radius: 30px;
+  width: 600px;
+  height: 450px;
+}
+
+.inputText {
+  width: 330px;
+  height: 55px;
+  border: solid 1px #515151;
+  border-radius: 5px;
+}
+</style>
