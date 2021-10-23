851b57db1785f858eccc1b3db77c03dc1e8e0bbb
ART <pakkawat.kh@gmail.com>
2021-10-24 00:45:16 +0700

    updated

--- src/pages/episode.vue
+++ src/pages/episode.vue
@@ -42,8 +42,17 @@
       <!-- Season management -->
       <q-dialog class="" v-model="dialogMainSeason" persistent>
         <q-card class="diaBox">
-          <div class="q-pt-md" style="font-size:24px;" align="center">
-            Season management
+          <div class="row">
+            <div class="col-1"></div>
+            <div class="q-pt-md col" style="font-size:24px;" align="center">
+              Season management
+            </div>
+            <div
+              class="col-1 q-pt-md cursor-pointer"
+              @click="closeSeasonMainDialog()"
+            >
+              <q-icon name="far fa-times-circle" size="24px" />
+            </div>
           </div>
           <div v-show="managementSeason != 0" class="seasonDiv">
             <div class="row q-pt-md">
@@ -56,8 +65,16 @@
             <div class="row" v-for="(item, index) in managementSeason">
               <div class="col-3  q-pl-md">{{ item.orderid }}</div>
               <div class="col-3 q-pl-md">{{ item.name }}</div>
-              <div class="col-3" align="center" ><span><u>Edit</u></span></div>
-              <div class="col-3" align="center"><u>Delete</u></div>
+              <div class="col-3" align="center">
+                <span class="cursor-pointer" @click="editSeason(item)"
+                  ><u>Edit</u></span
+                >
+              </div>
+              <div class="col-3" align="center">
+                <span class="cursor-pointer" @click="delSeason(item)"
+                  ><u>Delete</u></span
+                >
+              </div>
             </div>
           </div>
           <div class="addNewSeasonDiv cursor-pointer" @click="addNewSeason()">
@@ -100,6 +117,76 @@
           </div>
         </q-card>
       </q-dialog>
+      <!-- Edit season dialog -->
+      <q-dialog class="" v-model="dialogEditSeason" persistent>
+        <q-card class="diaBox2">
+          <div class="q-pt-md" style="font-size:24px;" align="center">
+            Edit season
+          </div>
+          <div class="row q-px-md q-pt-md">
+            <div class="col-3 q-pt-sm" align="center">Order ID</div>
+            <div class="col">
+              <q-input v-model="addSeason.orderid" type="number" dense />
+            </div>
+          </div>
+          <div class="row q-px-md q-pt-sm">
+            <div class="col-3 q-pt-sm" align="center">Name</div>
+            <div class="col">
+              <q-input v-model="addSeason.name" dense />
+            </div>
+          </div>
+          <div class="row q-pt-md">
+            <div style="width:80px;"></div>
+            <div
+              class="ynBtn q-ma-sm"
+              @click="closeEditSeason()"
+              align="center"
+            >
+              Cancel
+            </div>
+            <div
+              class="ynBtn q-ma-sm"
+              style="background-color:#ffc24c"
+              @click="editNewSeasonBtn()"
+              align="center"
+            >
+              Ok
+            </div>
+            <div style="width:80px;"></div>
+          </div>
+        </q-card>
+      </q-dialog>
+      <!-- Delete season confirm -->
+      <q-dialog class="" v-model="dialogConfirmDelSeason" persistent>
+        <q-card class="diaBox3">
+          <div class="q-pt-md" align="center">
+            <q-icon
+              name="fas fa-exclamation-circle"
+              size="48px"
+              style="color:#ffc24c"
+            />
+          </div>
+          <div class="q-px-md q-pt-md" align="center">
+            คุณต้องการลบ season นี้หรือไม่
+          </div>
+
+          <div class="row q-pt-md">
+            <div style="width:30px;"></div>
+            <div class="ynBtn q-ma-sm" @click="closeDelSeason()" align="center">
+              Cancel
+            </div>
+            <div
+              class="ynBtn q-ma-sm"
+              style="background-color:#ffc24c"
+              @click="delSeasonBtn()"
+              align="center"
+            >
+              Ok
+            </div>
+            <div style="width:30px;"></div>
+          </div>
+        </q-card>
+      </q-dialog>
     </div>
   </div>
 </template>
@@ -114,14 +201,79 @@ export default {
       selectSeason: "season 1",
       dialogMainSeason: false, //หน้าต่าง season
       dialogAddSeason: false, //หน้าต่างเพิ่ม season ใหม่
+      dialogEditSeason: false, //หน้าต่างแก้ไข season
+      dialogConfirmDelSeason: false, //หน้าต่าง confirm delete
       managementSeason: [], //ข้อมูล season
       addSeason: {
         orderid: "",
         name: ""
-      } //เพิ่มและแก้ไข season ใหม่
+      }, //เพิ่มและแก้ไข season ใหม่
+      editSeasonId: "", //แก้ไข Season
+      delSeasonId: "" //ลบ Season
     };
   },
   methods: {
+    //ลบ Season
+    delSeasonBtn() {
+      let data = {
+        id: this.delSeasonId
+      };
+      console.log(data);
+    },
+    //ปิดหน้าต่าง ลบ Season
+    closeDelSeason() {
+      this.dialogConfirmDelSeason = false;
+      this.dialogMainSeason = true;
+    },
+    //ลบ season
+    delSeason(item) {
+      this.delSeasonId = item.id;
+      this.dialogMainSeason = false;
+      this.dialogConfirmDelSeason = true;
+    },
+    //บันทึกแก้ไข season
+    async editNewSeasonBtn() {
+      //Check input
+      if (this.addSeason.orderid.length == 0) {
+        this.redNotify("Please input orderid");
+        return;
+      }
+      if (this.addSeason.name.length == 0) {
+        this.redNotify("Please input name");
+        return;
+      }
+
+      let data = {
+        id: this.editSeasonId,
+        orderid: this.addSeason.orderid,
+        seriesid: this.id,
+        name: this.addSeason.name
+      };
+
+      let url = this.serverpath + "bo_seriesmaineditseason.php";
+      let res = await axios.post(url, JSON.stringify(data));
+      this.greenNotify("Save completely");
+      this.dialogEditSeason = false;
+      this.loadSeason();
+      this.dialogMainSeason = true;
+    },
+    //ปิด season
+    closeEditSeason() {
+      this.dialogEditSeason = false;
+      this.dialogMainSeason = true;
+    },
+    //แก้ไข season
+    editSeason(item) {
+      this.editSeasonId = item.id;
+      this.addSeason.orderid = item.orderid;
+      this.addSeason.name = item.name;
+      this.dialogMainSeason = false;
+      this.dialogEditSeason = true;
+    },
+    //ปิด Dialog main season
+    closeSeasonMainDialog() {
+      this.dialogMainSeason = false;
+    },
     //เปิดหน้าต่าง Season
     async addSeasonBtn() {
       await this.loadSeason();
@@ -213,6 +365,12 @@ export default {
   max-width: 900px;
   width: 450px;
   height: 250px;
+  border-radius: 30px;
+}
+.diaBox3 {
+  max-width: 900px;
+  width: 350px;
+  height: 200px;
   border-radius: 30px;
 }
 .ynBtn {
